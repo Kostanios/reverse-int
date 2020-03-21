f8478fe2162a79f31c2c16f8f8cf92fdd289ef0e
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-21 21:36:20 +0300

    init:"start reverse-int task"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+    var stringint = String(n);
+    var recal = "";
   
+    for(var i = stringint.length-1; i >= 0 ; i--){
+        if(stringint[i]){
+            if(stringint[i]=="-"){
+                continue;
+                recal = "-" + recal;
+                
+            }
+        }
+      recal+=stringint[i];
+    }
+
+    return recal;
 }
